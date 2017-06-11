975643d7ba8d4e959aeeea9f7649ba6f1819e563
FoxKat <kfox3151@gmail.com>
2017-06-11 20:45:08 +0100

    Made Fizzez and Buzzez.

---
+++ FizzyBuzzyThing/FizzyBuzzyThing.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FizzyBuzzyThing
+{
+    class FizzyBuzzyThing
+    {
+        static void Main(string[] args)
+        {         
+            {
+                int daNumba = 1;
+
+                while (daNumba <= 100)
+                {
+                    string print;
+
+                    if (daNumba % 3 == 0 && daNumba % 5 == 0)
+                    {
+                        print = "FizzBuzz";
+                    }
+                    else if (daNumba % 3 == 0)
+                    {
+                        print = "Fizz";
+                    }
+                    else if (daNumba % 5 == 0)
+                    {
+                        print = "Buzz";
+                    }
+                    else
+                    {
+                        print = daNumba.ToString();
+                    }
+                    daNumba++;
+                    Console.WriteLine(print);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
